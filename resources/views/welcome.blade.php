24ad7b30f06a93c69335b91cd4c07e4202d63d60
Dimas Setiaji <dimassetiaji30@gmail.com>
2025-05-20 09:01:03 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Jenkins Update Test</title>
+        <title>Jenkins Update Test 123</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
